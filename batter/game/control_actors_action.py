a02f6e986b1bcec82ec82a2c65fe9cbb8a368d07
logan moffett <8loganm@gmail.com>
2021-02-24 20:50:12 -0700

    code will now take a and d keys as input to change the direction of the paddle

--- batter/game/control_actors_action.py
+++ batter/game/control_actors_action.py
@@ -26,6 +26,7 @@ class ControlActorsAction(Action):
         Args:
             cast (dict): The game actors {key: tag, value: list}.
         """
+
         direction = self._input_service.get_direction()
-        robot = cast["robot"][0] # there's only one in the cast
-        robot.set_velocity(direction)        
+        paddle = cast["paddle"][0] # there's only one in the cast
+        paddle.set_velocity(direction)        
